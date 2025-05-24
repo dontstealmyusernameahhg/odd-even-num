5b0368eba33b676c59ab7170300ada7b60cb3e16
Buttan <poonam.buttan@ingrammicro.com>
2025-05-24 16:25:18 +1200

    ok

---
+++ raj.py
@@ -0,0 +1,10 @@
+birthdays = {
+      "Poopoopp": "April 1",
+      "Bob": "December 12",
+      "Charlie": "March 4",
+      "David": "January 1",
+      "Eve": "February 14"
+}
+print("Raj's friend's birthdays:")
+for name, date in birthdays.items():
+      print(name + "'s birthday is on " + date)
