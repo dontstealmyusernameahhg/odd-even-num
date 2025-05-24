ee92761e89a44f31291cbbfe61ed73b93f2ef02e
Buttan <poonam.buttan@ingrammicro.com>
2025-05-24 16:08:37 +1200

    first commit

---
+++ main2.py
@@ -0,0 +1,11 @@
+birthdays = {
+    "Poo ": "1 january" ,
+    "Simple ": "1 january" ,
+    "Kaka ": "1 january" ,
+    "Kako ": "1 january" ,
+    "Kakp ": "1 january"
+}
+
+print("Raj's friends birthdays:")
+for name, date in birthdays. items():
+    print (name + "birthday is on" + date)
